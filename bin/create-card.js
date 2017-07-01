8359a667738e43847f992f37ad4910a26a8c0546
Yuriy Baturin <baturinyuya@altarix.ru>
2017-07-01 13:17:36 +0500

    fix

--- bin/create-card.js
+++ bin/create-card.js
@@ -3,6 +3,6 @@
 const fs = require('fs');
 const path = require('path');
 const componentName = process.argv[2];
-const CardCreator = require('../lib/index.js');
+const CardCreator = require('../lib/card-creator.js');
 
 (new CardCreator).create(componentName);
